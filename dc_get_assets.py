3613f410edf5ca603c8146250745745f84043cd2
shir22 <shir@deepchecks.com>
2023-03-16 13:51:33 +0200

    improved inference processing pipeline with numpy

--- dc_get_assets.py
+++ dc_get_assets.py
@@ -59,6 +59,7 @@ def get_train_ds():
 def get_test_ds():
     download_titanic_file(TEST_FILENAME)
     test_data = pd.read_csv(TEST_FILE)
+    test_data['Survived'] = test_data['Survived'].sample(frac=1)
     test_ds = dct.Dataset(test_data, **dataset_metadata)
     return test_ds
 
