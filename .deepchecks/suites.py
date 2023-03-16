94174fc0844305b7d53b02a96014087e2a9d2f8b
shir22 <shir@deepchecks.com>
2023-03-16 13:48:47 +0200

    update suites for ci-cd

--- .deepchecks/suites.py
+++ .deepchecks/suites.py
@@ -5,6 +5,20 @@ from deepchecks.tabular.checks import (DatasetsSizeComparison, DateTrainTestLeak
                                        MultivariateDrift,
                                        NewCategoryTrainTest, NewLabelTrainTest, StringMismatchComparison,
                                        TrainTestSamplesMix)
+
+# import all:
+
+from deepchecks.tabular.checks import (BoostingOverfit, CalibrationScore, ConflictingLabels, ConfusionMatrixReport,
+                                       DataDuplicates, DatasetsSizeComparison, DateTrainTestLeakageDuplicates,
+                                       DateTrainTestLeakageOverlap, FeatureDrift, FeatureFeatureCorrelation,
+                                       FeatureLabelCorrelation, FeatureLabelCorrelationChange,
+                                       IdentifierLabelCorrelation, IndexTrainTestLeakage, IsSingleValue, LabelDrift,
+                                       MixedDataTypes, MixedNulls, ModelInferenceTime, MultivariateDrift,
+                                       NewCategoryTrainTest, NewLabelTrainTest, OutlierSampleDetection, PercentOfNulls,
+                                       PredictionDrift, RegressionErrorDistribution, RocReport, SimpleModelComparison,
+                                       SingleDatasetPerformance, SpecialCharacters, StringLengthOutOfBounds,
+                                       StringMismatch, StringMismatchComparison, TrainTestPerformance,
+                                       TrainTestSamplesMix, UnusedFeatures, WeakSegmentsPerformance)
 from deepchecks.tabular.suites import model_evaluation
 
 
@@ -27,5 +41,24 @@ def first_custom_suite():
             MultivariateDrift(**kwargs).add_condition_overall_drift_value_less_than(),
         )
 
-def my_model_evaluation():
+def my_full_model_evaluation():
     return model_evaluation()
+
+
+def my_model_evaluation():
+    kwargs = {}
+    return Suite(
+        'My Model Evaluation Suite',
+        TrainTestPerformance(**kwargs).add_condition_train_test_relative_degradation_less_than(),
+        RocReport(**kwargs).add_condition_auc_greater_than(),
+        ConfusionMatrixReport(**kwargs),
+        PredictionDrift(**kwargs).add_condition_drift_score_less_than(),
+        SimpleModelComparison(**kwargs).add_condition_gain_greater_than(),
+        # WeakSegmentsPerformance(**kwargs).add_condition_segments_relative_performance_greater_than(),
+        CalibrationScore(**kwargs),
+        RegressionErrorDistribution(
+            **kwargs).add_condition_kurtosis_greater_than().add_condition_systematic_error_ratio_to_rmse_less_than(),
+        UnusedFeatures(**kwargs).add_condition_number_of_high_variance_unused_features_less_or_equal(),
+        BoostingOverfit(**kwargs).add_condition_test_score_percent_decline_less_than(),
+        ModelInferenceTime(**kwargs).add_condition_inference_time_less_than(),
+    )
