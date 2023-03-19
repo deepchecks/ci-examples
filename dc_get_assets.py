16dbd628390ae0c77cbd03842cf7046046912c80
shir22 <shir@deepchecks.com>
2023-03-19 15:55:09 +0200

    fix numpy scrambling

--- dc_get_assets.py
+++ dc_get_assets.py
@@ -63,7 +63,7 @@ def get_test_ds():
     test_data = pd.read_csv(TEST_FILE)
     # demonstrating that for for optimization, numpy was used for processing in pipeline,
     # and as last step - inserted back to dataframe
-    test_data['Survived'] = np.array(test_data['Survived'].sample(frac=1))
+    # test_data['Survived'] = np.array(test_data['Survived'].sample(frac=1))
     test_ds = dct.Dataset(test_data, **dataset_metadata)
     return test_ds
 
